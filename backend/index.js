9bd1f2b6d4137bde0ae29b442fe1f692a494b8b8
ankit-mego <64592422+ankit-mego@users.noreply.github.com>
2023-01-18 13:38:33 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express=require('express');
+const app=express();
+const adminroute= require('./router/apiadmincheck');
+const transroute=require('./router/transaction')
+const generateroute=require('./router/generator')
+const student=require('./router/student')
+const auth=require('./router/auth')
+const mail=require('./router/mail')
+app.use(
+    express.urlencoded({
+        extended:true
+    })
+)
+app.use(
+    function(req,res,next)
+    {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    }
+)
+app.use(
+    express.json()
+)
+const temproute=require('./router/template');
+app.use('/template',temproute)
+app.use('/admin',adminroute)
+app.use('/trans',transroute)
+app.use('/generate',generateroute)
+app.use('/student',student)
+app.use('/auth',auth)
+app.use('/mail',mail)
+app.listen(4000);
